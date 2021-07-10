f559b0234538ab73f3dd7b0e579bd775489f9a88
saurabhraj123 <me4saurabh4u@gmail.com>
2021-07-11 01:00:29 +0530

    Appointment Details visible on Profile

--- routes/form.js
+++ routes/form.js
@@ -87,11 +87,25 @@ router.get('/:id', check_form, (req, res) => {
         var decodedValue = decode(req.cookies.jwt);
 
     if(req.cookies.jwt){
-        db.query(`SELECT * FROM ${decodedValue.user_type}s JOIN registrations USING(registration_id) WHERE registration_id = ${decodedValue._id} `, (err, result) => {
-            if(err) return res.send(err);
-            console.log(result[0]);
-            console.log('Url Query: ' + req.query.booked);
-            res.render('profile_user', {isValidated:true, result:result[0], booked: req.query.booked});
+        db.query(`SELECT * FROM ${decodedValue.user_type}s JOIN registrations USING(registration_id) WHERE registration_id = ${decodedValue._id}`, (err, result) => {
+
+            if(err) return res.send('Error in first query..');
+
+            db.query(`SELECT d.first_name, d.last_name, a.appointment_date, a.status, a.appointment_id FROM ${decodedValue.user_type}s JOIN registrations USING(registration_id) JOIN appointment a ON ${decodedValue.user_type}s.registration_id = a.${decodedValue.user_type}s_registration_id JOIN doctors d ON a.doctors_registration_id = d.registration_id WHERE ${decodedValue.user_type}s.registration_id = ${decodedValue._id} ORDER BY a.appointment_id DESC `, (err2, result2) => {
+                
+                if(err2) return res.send(err2);
+
+                console.log(result[0]);
+                console.log("RESULT: " + result2);
+                // if(typeof(result2 == 'undefined'))
+                //     result2 = [];
+                console.log("RESULT: " + result2);
+                // res.send(result2);
+                
+                console.log('Url Query: ' + req.query.booked);
+                res.render('profile_user', {isValidated:true, result:result[0], results: result2, booked: req.query.booked});
+            })
+            
         })
         
     }else
